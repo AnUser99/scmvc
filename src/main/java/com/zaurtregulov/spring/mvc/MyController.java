6331d579281175be9db302102c776d647619921e
AnUser99 <kisamasyanya@gmail.com>
2023-03-02 21:23:31 +0300

    04066

--- src/main/java/com/zaurtregulov/spring/mvc/MyController.java
+++ src/main/java/com/zaurtregulov/spring/mvc/MyController.java
@@ -2,11 +2,13 @@ package com.zaurtregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 //@RequestMapping("/employee")
@@ -29,12 +31,16 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+    public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp, BindingResult bindingResult) {
 
-        emp.setName("Mr." + emp.getName());
-        emp.setSurname(emp.getSurname() + "!");
+//        System.out.println("surname length = " + emp.getSurname().length());
+
+        return bindingResult.hasErrors() ? "ask-emp-details-view" : "show-emp-details-view";
+
+//        emp.setName("Mr." + emp.getName());
+//        emp.setSurname(emp.getSurname() + "!");
 //        emp.setSalary(emp.getSalary() * 10);
 
-        return "show-emp-details-view";
+//        return "show-emp-details-view";
     }
 }
